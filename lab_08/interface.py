1efcc6663092e4676b63f9f9719e41a8295c6252
Ekaterina Ambartsumova <ekaterina.amba@mail.ru>
2023-05-14 23:09:40 +0300

    modfified interface.py in lab_08

--- lab_08/interface.py
+++ lab_08/interface.py
@@ -33,13 +33,14 @@ class CanvasFrame(MainFrame):
         self.line_color = 'black'
 
         # config Canvas to be scrollable
-        self.make_canva_scrollable()
+        #self.make_canva_scrollable()
 
         self.canvas_interface()
 
 
     def canvas_interface(self):
         #self.canva.pack(fill='both', expand=True, side='left')
+        self.canva.pack(side='left', expand=True, fill='both')
         self.canva.config(bg = 'white')
         self.redraw_canva()
 
@@ -55,7 +56,7 @@ class CanvasFrame(MainFrame):
         vbar.config(command=self.canva.yview)
 
         self.canva.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
-        self.canva.pack(side='left', expand=True, fill='both')
+
 
 
     def get_canva(self):
@@ -80,7 +81,7 @@ class CanvasFrame(MainFrame):
 
     def redraw_canva(self):
         self.canva.delete("all")
-        self.draw_axes()
+        #self.draw_axes()
 
     def get_point_canva(self, event):
         x = event.x
@@ -118,7 +119,7 @@ class ColorFrame(OptionFrame):
         self.text_color = text_color
 
         self.line_color = 'black'
-        self.cut_color = "blue"
+        self.cut_color = "light green"
         self.vis_color = 'red'
 
 
@@ -127,21 +128,21 @@ class ColorFrame(OptionFrame):
                                    font=Font, fg= text_color)
         self.line_label = tk.Label(self.color_line_frame, bg=self.color, text="Текущий цвет отрезка:",
                                    fg=text_color,   font=Font)
-        self.cur_line_color_label = tk.Label(self.color_line_frame, bg="black")
+        self.cur_line_color_label = tk.Label(self.color_line_frame, bg=self.line_color)
 
         self.color_visline_frame = tk.Frame(self, bg=self.color)
         self.chng_visline = tk.Button(self.color_visline_frame, text='Изменить цвет видимых', bg=self.color,
                                      font=Font, fg=text_color)
         self.visline_label = tk.Label(self.color_visline_frame, bg=self.color, text="Текущий цвет видимых:",
                                      fg=text_color, font=Font)
-        self.cur_visline_color_label = tk.Label(self.color_visline_frame, bg="red")
+        self.cur_visline_color_label = tk.Label(self.color_visline_frame, bg=self.vis_color)
 
         self.cutting_color_frame = tk.Frame(self, bg=self.color)
         self.chng_cutting = tk.Button(self.cutting_color_frame, text='Изменить цвет отсекателя', bg=self.color,
                                       font=Font, fg=text_color)
         self.cutting_label = tk.Label(self.cutting_color_frame, bg=self.color, text="Текущий цвет отсекателя",
                                       fg=text_color, font=Font)
-        self.cur_filling_color_label = tk.Label(self.cutting_color_frame, bg="black")
+        self.cur_filling_color_label = tk.Label(self.cutting_color_frame, bg=self.cut_color)
 
         self.color_interface()
 
@@ -169,7 +170,8 @@ class ColorFrame(OptionFrame):
         self.vis_color = color
 
     def set_cut_color(self, color):
-        self.line_color = color
+        self.cut_color = color
+        self.cur_filling_color_label.config(bg = self.cut_color)
 
 
     def get_line_color(self):
